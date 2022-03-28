d718e985c1ea6d60cd1bd926adbd9515fb0bd1e0
Hakaino <marco_alemao@hotmail.com>
2022-03-28 15:36:14 +0200

    can print box for person

--- src/prosthetic_hand/src/camera_node.cpp
+++ src/prosthetic_hand/src/camera_node.cpp
@@ -7,28 +7,12 @@
 #include <librealsense2/rs.hpp>
 #include <yolact_ros_msgs/Detections.h>
 
-void rgbCallback(const sensor_msgs::ImageConstPtr& msg){
-    try{
 
-        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_8UC3);
-        cv::Mat rgb_img = cv_ptr->image;
-        cv::Mat rgb_img_show = cv_ptr->image;
+cv::Mat rgb_img;
+int width = 640;    // hardcodding... to make it faster
+int height = 480;
 
-        //calculate the distance
-        int w = 320;
-        int h = 240;
-        int offset = 30;
-        cv::Point pt1(w, h);
-        cv::Point pt2(w + offset, h + offset);
-        cv::rectangle(rgb_img_show, pt1, pt2, 65535, 10);
-
-        cv::imshow("RGB", rgb_img_show);
-        cv::waitKey(30);
-    }
-    catch (cv_bridge::Exception& e){
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-    }
-}
+cv::Mat m1 = cv::Mat::zeros(height, width, CV_8UC1); // test masks
 
 void pointCloudCallback(const sensor_msgs::ImageConstPtr& msg){
     try{
@@ -44,13 +28,11 @@ void pointCloudCallback(const sensor_msgs::ImageConstPtr& msg){
                 depth_show.at<int>(i, j) = depth.at<int>(i, j) / pow(10, 5) * 65535;
             }
         }
-
-
-        int w = depth.cols;
-        int h = depth.rows;
-        int offset = 30;
-        int point = depth.at<int>((w+offset)/2, (h+offset)/2);
-        ROS_INFO("distance: %d", point);    //TODO: must calculate unities
+        //width = depth.cols;
+        //height = depth.rows;
+        //int offset = 30;
+        //int point = depth.at<int>((width+offset)/2, (height+offset)/2);
+        //ROS_INFO("width:%d height:%d", width, height);    //TODO: must calculate unities
 
         /*//calculate the distance
         cv::Point pt1(w, h);
@@ -69,11 +51,10 @@ void detectionsCallback(const yolact_ros_msgs::Detections &detec){
     //yolact_ros_msgs::Detection_<std::allocator<void> > foo = detec.detections[20];
     //std::cout << name << std::endl;
     //ROS_INFO("Msg: " << &name);
+    //if (rgb_img != NULL) {
     for(int i=0; i<5; i++){ // only 5 detections per message for some reason
       std::string class_name = detec.detections[i].class_name;
-
-      float score = detec.detections[i].score; // its float 64, it may cahnge in a diff machine must controll it later
-
+      float score = detec.detections[i].score; // char64_t its float 64, it may cahnge in a diff machine must controll it later
       std::vector<int_least32_t> box = {
         detec.detections[i].box.x1,
         detec.detections[i].box.y1,
@@ -81,85 +62,51 @@ void detectionsCallback(const yolact_ros_msgs::Detections &detec){
         detec.detections[i].box.y2
       };
 
+
       std::vector<int> size = {
         detec.detections[i].mask.width,
         detec.detections[i].mask.height
       };
 
+      //np.zeros([height, width, 3], dtype=np.uint8)
+      //std::cout << (size[0] <= width) << " " << (size[1] <= height) << std::endl;
 
-      //cv::Mat img = cv::Mat::ones(height, width, CV_8UC1)*250;
-      //cv::Mat img = cv::Mat::create(mask, height, width, CV_8UC1);
+      if (class_name == "person") {
+        m1 = cv::Mat::zeros(height, width, CV_8UC1);
+        for (size_t i = box[0]; i < box[2]-1; i++) {
+          for (size_t j = box[1]; j < box[3]-1; j++) {
+            m1.data[(j*width) + i] = 250;
 
-      std::cout << box[0] << " " << box[1] << " " << box[2] << " " << box[3] << " " << size[0] <<" "<< size[1] << " "<<  class_name <<std::endl;
+            //rgb_img.data[rgb_img.step[0]*i + rgb_img.step[1]* j + 0] = 0;
+            //rgb_img.data[rgb_img.step[0]*i + rgb_img.step[1]* j + 1] = 0;
+            //rgb_img.data[rgb_img.step[0]*i + rgb_img.step[1]* j + 2] = 0;
+          };
+        };
+        //m1.data[200] = 254;
+        //std::cout << width << " " << height << '\n';
+        cv::imshow("mask", m1);
+        //cv::waitKey(30);
+      };
 
-
-      //cv::Mat img(width, height, CV_8UC1);
-      //cv::cvtColor(mask, img, CV_8UC1);
-      //img = img.reshape(width, height);
-      //cv::imshow("mask", img);
-
-
-      //for(int i=0; i<width; ++i){
-        //for(int j=0; j<height; ++j){
-          //img.at<uint8_t>(i, j) = 255; //mask[i*j];
-        //}
+      //std::cout << box[0] << " " << box[1] << " " << box[2] << " " << box[3] << " " << size[0] <<" "<< size[1] << " "<<  class_name <<std::endl;
+      //if (rgb_img.data[10]) {
+        //std::cout << rgb_img.data[10] << '\n';
+        //cv::imshow("mask", rgb_img);
+        //cv::waitKey(30);
       //}
-
-      //Mat img(width, height, CV_8UC1, Scalar(0,0, 100));
-      //std::cout << height << "\t" << width << " " << sizeof(mask) << std::endl;
-
-      //cv::Mat img(width, height, cv::CV_64FC1);
-      //for(int i=0; i<width; ++i){
-        //for(int j=0; j<height; ++j){
-          //img.at<auto>(i, j) = mask.at(i).at(j);
-        //}
-      //}
-
-
-
-      //cv::Mat binary = cv::imdecode(detec.detections[i].mask.mask, cv::IMREAD_ANYCOLOR);
-
-      //cv::waitKey(30);
-
-      //cv::Mat matAngles(angles.size(), angles.at(0).size(), sensor_msgs::image_encodings::TYPE_8UC1);
-//for(int i=0; i<matAngles.rows; ++i)
-     //for(int j=0; j<matAngles.cols; ++j)
-          //matAngles.at<double>(i, j) = angles.at(i).at(j);
-
-
-
-
-
-      //cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(mask, sensor_msgs::image_encodings::TYPE_8UC1);
-      //cv_bridge::CvImagePtr cv_ptr = cv::imdecode (mask, sensor_msgs::image_encodings::TYPE_8UC1)
-      //cv::Mat binary = cv_ptr->image;
-      //cv::Mat rgb_img_show = cv_ptr->image;
-
-      //if (name=="person") {
-      //cv::imshow("mask", binary);
-      //cv::waitKey(30);
-      //}
-
-      //std::cout << i << "\t"<< name << certainty << std::endl;
-
-    }
-
-    //TODO: iterate through detections
-    //TODO: get mask-class pair
+    };
     //TODO: overlap masks to point cloud
     //TODO: remove further points
-
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     ros::init(argc, argv, "camera_node");
+    //ros::Rate r(5); // 5 hz
     ros::NodeHandle nh;
     cv::startWindowThread();
-    //image_transport::ImageTransport it(nh);
-    //image_transport::Subscriber subD = it.subscribe("/camera/aligned_depth_to_color/image_raw", 1, pointCloudCallback);
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber subD = it.subscribe("/camera/aligned_depth_to_color/image_raw", 1, pointCloudCallback);
     ros::Subscriber subDet = nh.subscribe("/yolact_ros/detections", 1, detectionsCallback);
-
     ros::spin();
     cv::destroyWindow("view");
 }
