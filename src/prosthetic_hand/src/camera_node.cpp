9834a295cd646f4cdb21a3e098a3dba1fd400a97
Hakaino <marco_alemao@hotmail.com>
2022-03-28 10:30:56 +0200

    can get all the data from detections

--- src/prosthetic_hand/src/camera_node.cpp
+++ src/prosthetic_hand/src/camera_node.cpp
@@ -65,12 +65,90 @@ void pointCloudCallback(const sensor_msgs::ImageConstPtr& msg){
     }
 }
 
-void detectionsCallback(const yolact_ros_msgs::Detections &detections){
-
-    yolact_ros_msgs::Detection_<std::allocator<void> > foo = detections.detections[0];//.class_name;
-    std::string name = foo.class_name;
-    std::cout << name << std::endl;
+void detectionsCallback(const yolact_ros_msgs::Detections &detec){
+    //yolact_ros_msgs::Detection_<std::allocator<void> > foo = detec.detections[20];
+    //std::cout << name << std::endl;
     //ROS_INFO("Msg: " << &name);
+    for(int i=0; i<5; i++){ // only 5 detections per message for some reason
+      std::string class_name = detec.detections[i].class_name;
+
+      float score = detec.detections[i].score; // its float 64, it may cahnge in a diff machine must controll it later
+
+      std::vector<int_least32_t> box = {
+        detec.detections[i].box.x1,
+        detec.detections[i].box.y1,
+        detec.detections[i].box.x2,
+        detec.detections[i].box.y2
+      };
+
+      std::vector<int> size = {
+        detec.detections[i].mask.width,
+        detec.detections[i].mask.height
+      };
+
+
+      //cv::Mat img = cv::Mat::ones(height, width, CV_8UC1)*250;
+      //cv::Mat img = cv::Mat::create(mask, height, width, CV_8UC1);
+
+      std::cout << box[0] << " " << box[1] << " " << box[2] << " " << box[3] << " " << size[0] <<" "<< size[1] << " "<<  class_name <<std::endl;
+
+
+      //cv::Mat img(width, height, CV_8UC1);
+      //cv::cvtColor(mask, img, CV_8UC1);
+      //img = img.reshape(width, height);
+      //cv::imshow("mask", img);
+
+
+      //for(int i=0; i<width; ++i){
+        //for(int j=0; j<height; ++j){
+          //img.at<uint8_t>(i, j) = 255; //mask[i*j];
+        //}
+      //}
+
+      //Mat img(width, height, CV_8UC1, Scalar(0,0, 100));
+      //std::cout << height << "\t" << width << " " << sizeof(mask) << std::endl;
+
+      //cv::Mat img(width, height, cv::CV_64FC1);
+      //for(int i=0; i<width; ++i){
+        //for(int j=0; j<height; ++j){
+          //img.at<auto>(i, j) = mask.at(i).at(j);
+        //}
+      //}
+
+
+
+      //cv::Mat binary = cv::imdecode(detec.detections[i].mask.mask, cv::IMREAD_ANYCOLOR);
+
+      //cv::waitKey(30);
+
+      //cv::Mat matAngles(angles.size(), angles.at(0).size(), sensor_msgs::image_encodings::TYPE_8UC1);
+//for(int i=0; i<matAngles.rows; ++i)
+     //for(int j=0; j<matAngles.cols; ++j)
+          //matAngles.at<double>(i, j) = angles.at(i).at(j);
+
+
+
+
+
+      //cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(mask, sensor_msgs::image_encodings::TYPE_8UC1);
+      //cv_bridge::CvImagePtr cv_ptr = cv::imdecode (mask, sensor_msgs::image_encodings::TYPE_8UC1)
+      //cv::Mat binary = cv_ptr->image;
+      //cv::Mat rgb_img_show = cv_ptr->image;
+
+      //if (name=="person") {
+      //cv::imshow("mask", binary);
+      //cv::waitKey(30);
+      //}
+
+      //std::cout << i << "\t"<< name << certainty << std::endl;
+
+    }
+
+    //TODO: iterate through detections
+    //TODO: get mask-class pair
+    //TODO: overlap masks to point cloud
+    //TODO: remove further points
+
 }
 
 int main(int argc, char **argv)
