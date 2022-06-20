c23a3df6d823256a185f5ac531e501f48d5494ed
Hakaino <marco_alemao@hotmail.com>
2022-06-20 15:45:26 +0200

    correct some bugs

--- src/prosthetic_hand/src/camera_node.cpp
+++ src/prosthetic_hand/src/camera_node.cpp
@@ -25,6 +25,59 @@ image_transport::Publisher people_pub;
 image_transport::Publisher unknown_object_pub;
 //ros::Publisher feedback_pub;
 
+sensor_msgs::ImagePtr handle_unknown_object(const yolact_ros_msgs::Detection &detection){
+    //mask_img = cv::Mat::ones(height, width, CV_8UC1) * 255;
+    cv::Mat clean_depth = depth;
+
+    float upper_threshold = 400; // in millimeters
+    float lower_threshold = 200;
+
+    std::vector<int> size = {
+            detection.mask.width,
+            detection.mask.height
+    };
+
+
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            uint16_t z = clean_depth.at<uint16_t>(y, x);
+            if ((z < lower_threshold) || (upper_threshold < z)){
+                clean_depth.at<uint16_t>(y, x) = 0;
+                //mask_img.at<uint8_t>(y, x) = 0;
+            }else if ((detection.box.x1 < x) && (x < detection.box.x2) && (detection.box.y1 < y) && (y < detection.box.y2)){
+                size_t index = (y-detection.box.y1) * size[0] + (x-detection.box.x1);
+                size_t byte_ind = index / 8;
+                size_t bit_ind = 7 - (index % 8);  // bitorder 'big'
+                size_t val = detection.mask.mask[byte_ind] & (1 << bit_ind);
+                if (val){  //assuming object in hand
+                    clean_depth.at<uint16_t>(y, x) = 0;
+                    //mask_img.at<uint8_t>(y, x) = 0;
+                    //ROS_INFO("z: %d", depth.at<uint16_t>(y, x));
+                }
+            }
+        }
+    }
+    //cv::imshow("unknown object", mask_img);
+    //feedback_pub.publish(1);  // 1 is the next logical state for the state_machine
+    //ROS_INFO(depth2point(clean_depth)); //saves the point cloud to a file
+    return cv_bridge::CvImage(std_msgs::Header(), "mono16", clean_depth).toImageMsg();
+}
+
+
+void detectionsCallback(const yolact_ros_msgs::Detections &detec) {
+    //ROS_INFO("Detection!");
+    for (int i = 0; i < 5; i++) {  // only 5 detections per message for some reason
+        std::string class_name = detec.detections[i].class_name;
+        float score = detec.detections[i].score;  // char64_t its float 64, it may cahnge in a diff machine must controll it later
+        if ((class_name == "person") && (score > .5)) {
+            //people_pub.publish(handle_detections(detec.detections[i]));
+            unknown_object_pub.publish(handle_unknown_object(detec.detections[i])); // this assumes only one person detection
+            //ROS_INFO("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        }
+    //TODO: remove further points
+        }
+    }
+
 void pointCloudCallback(const sensor_msgs::ImageConstPtr &msg) {
     try {
         cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);
@@ -40,6 +93,7 @@ void pointCloudCallback(const sensor_msgs::ImageConstPtr &msg) {
     }
 }
 
+/*
 sensor_msgs::ImagePtr handle_detections(const yolact_ros_msgs::Detection &detection){
     size_t index;
     size_t byte_ind;
@@ -78,65 +132,6 @@ sensor_msgs::ImagePtr handle_detections(const yolact_ros_msgs::Detection &detect
     return cv_bridge::CvImage(std_msgs::Header(), "mono16", clean_depth).toImageMsg();
 }
 
-sensor_msgs::ImagePtr handle_unknown_object(const yolact_ros_msgs::Detection &detection){
-    //mask_img = cv::Mat::ones(height, width, CV_8UC1) * 255;
-    cv::Mat clean_depth = depth;
-
-    float upper_threshold = 400; // in millimeters
-    float lower_threshold = 200;
-
-    std::vector <uint32_t> box = {
-            detection.box.x1,
-            detection.box.y1,
-            detection.box.x2,
-            detection.box.y2
-    };
-
-    std::vector<int> size = {
-            detection.mask.width,
-            detection.mask.height
-    };
-
-
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-            uint16_t z = clean_depth.at<uint16_t>(y, x);
-            if ((z < lower_threshold) || (upper_threshold < z)){
-                clean_depth.at<uint16_t>(y, x) = 0;
-                //mask_img.at<uint8_t>(y, x) = 0;
-            }else if ((box[0] < x) && (x < box[2]) && (box[1] < y) && (y < box[3])){
-                size_t index = (y-box[1]) * size[0] + (x-box[0]);
-                size_t byte_ind = index / 8;
-                size_t bit_ind = 7 - (index % 8);  // bitorder 'big'
-                size_t val = detection.mask.mask[byte_ind] & (1 << bit_ind);
-                if (val){  //assuming object in hand
-                    clean_depth.at<uint16_t>(y, x) = 0;
-                    //mask_img.at<uint8_t>(y, x) = 0;
-                    //ROS_INFO("z: %d", depth.at<uint16_t>(y, x));
-                }
-            }
-        }
-    }
-    //cv::imshow("unknown object", mask_img);
-    //feedback_pub.publish(1);  // 1 is the next logical state for the state_machine
-    ROS_INFO(depth2point(clean_depth)); //saves the point cloud to a file
-    return cv_bridge::CvImage(std_msgs::Header(), "mono16", clean_depth).toImageMsg();
-}
-
-void detectionsCallback(const yolact_ros_msgs::Detections &detec) {
-    //ROS_INFO("Detection!");
-    for (int i = 0; i < 5; i++) {  // only 5 detections per message for some reason
-        std::string class_name = detec.detections[i].class_name;
-        float score = detec.detections[i].score;  // char64_t its float 64, it may cahnge in a diff machine must controll it later
-        if ((class_name == "person") && (score > .5)) {
-            //people_pub.publish(handle_detections(detec.detections[i]));
-            unknown_object_pub.publish(handle_unknown_object(detec.detections[i])); // this assumes only one person detection
-            //ROS_INFO("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-        }
-    //TODO: remove further points
-        }
-    }
-
 void state_update(const std_msgs::UInt8 &state){
   //machine_state = state;
 }
@@ -167,10 +162,10 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr depth2point(){
     }
   }
   //int pcl::FileWriter::write 	save_success(const std::string &"cloud_from_camera.ply", const pcl::PointCloud<PointXYZ> &cloud_pointer)
-  pcl::io::savePCDFileASCII ("cloud_from_camera.pcd", &cloud);
+  //pcl::io::savePCDFileASCII ("cloud_from_camera.pcd", &cloud);
   return cloud_pointer;
 }
-
+*/
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "camera_node");
@@ -180,11 +175,11 @@ int main(int argc, char **argv) {
     ROS_INFO("Camera node starts!-----------------------------------------");
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber subD = it.subscribe("camera/aligned_depth_to_color/image_raw", 1, pointCloudCallback);
-    //people_pub = it.advertise("people_depth", 1);
+    ros::Subscriber subDet = nh.subscribe("/yolact_ros/detections", 1, detectionsCallback);
     unknown_object_pub = it.advertise("unknown_depth", 1);
+    //people_pub = it.advertise("people_depth", 1);
     //feedback_pub = nh.advertise<std_msgs::UInt8>("state_feedback", 1);
     //usb_cam/image_raw", 1, pointCloudCallback);
-    ros::Subscriber subDet = nh.subscribe("/yolact_ros/detections", 1, detectionsCallback);
     //ros::Subscriber state_sub = nh.subscribe("/state_feedforward", 1, state_update);
     ros::spin();
     cv::destroyWindow("view");
